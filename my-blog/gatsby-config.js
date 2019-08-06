618167f40fd2a0b2162028d62378b52c8e1bcb3f
jeandavidwuilquot <david.wuilquot@gmail.com>
2019-08-06 16:18:55 +0200

    gatsby

---
+++ my-blog/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `React for dummies`,
+    author: `Dax`,
+    description: `React Tutorial`,
+    social: [
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/jean-david-wuilquot/`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/jeandavidwuilquot?tab=repositories`,
+      },
+    ],
+  },
+}
---
+++ my-blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Introduction à React.js proposée par <Styled.a href="http://example.com/">Dax</Styled.a>
+    {` `}
+    .
+    <br />
+    Je vais vous présenter les bases de ce language.
+  </Fragment>
+)
